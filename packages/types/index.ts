7715e3d7a2af615728760ae445c03c0193c5a187
Italo Alfaro <bestor911@gmail.com>
2022-02-23 21:28:39 -0500

    feat: new types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -17,3 +17,45 @@ export interface User {
 export interface DataID {
   watchId: number | null;
 }
+
+export interface Coord {
+  lat: number;
+  lng: number;
+}
+
+export interface PolyPath {
+  path: Coord[];
+  color: string;
+}
+
+export interface WazeTrafficInfo {
+  city: string;
+  date: number;
+  description: string;
+  level: number;
+  speedKh: number;
+  time: number;
+  street: string;
+  path: Coord[];
+}
+
+export interface WazeAlertInfo {
+  city: string;
+  date: number;
+  description: string;
+  location: Coord;
+  street: string;
+  type: string;
+}
+
+export interface InfoWindowData {
+  location: Coord;
+  description: string;
+  date: number;
+  street: string;
+  city: string;
+  level?: number;
+  speedKh?: number;
+  type?: string;
+  time?: number;
+}
