ea8bf2f1522d52cc51026cd2e67ed6774fd92a77
Italo Alfaro <bestor911@gmail.com>
2022-03-05 17:32:06 -0500

    feat: path route

--- apps/api/src/routes/index.routes.ts
+++ apps/api/src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { index } from "../controllers";
 import risk from "./risk.routes";
 import report from "./report.routes";
+import path from "./path.routes";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.get("/", index);
 router.use("/risk", risk);
 
 router.use("/report", report);
+
+router.use("/path", path);
 
 export default router;
