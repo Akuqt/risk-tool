0d7b0c6436b90287ca8ee66882f81afab4115106
Italo Alfaro <bestor911@gmail.com>
2022-03-16 11:16:58 -0500

    refactor(api): remove unnecessary auth response data

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -22,35 +22,81 @@ export const signIn = async (
       .populate("role")
       .populate("company");
 
-    if (!_driver || _driver?.role.name !== type) return res.json({ ok: false });
+    if (!_driver) return res.status(401).json({ ok: false });
 
     const matchPass = await comparePassword(_driver.password, password);
 
-    if (!matchPass) return res.json({ ok: false });
+    if (!matchPass) return res.status(401).json({ ok: false });
 
     res.cookie("jid", createRefreshToken(_driver), cookieConf);
 
-    return res.json({ data: _driver, token: createAcessToken(_driver) });
+    return res.json({
+      ok: true,
+      result: {
+        data: {
+          name: _driver.name,
+          id: _driver._id,
+          username: _driver.username,
+          plate: _driver.plate,
+          lat: _driver.lat,
+          lng: _driver.lng,
+          role: _driver.role.name,
+          company: {
+            name: _driver.company.name,
+            id: _driver.company._id,
+            address: _driver.company.address,
+            lat: _driver.company.lat,
+            lng: _driver.company.lng,
+          },
+        },
+        token: createAcessToken(_driver),
+      },
+    });
   }
 
   if (type === "company") {
-    const _company: IDriver | null = await CompanyModel.findOne({
+    const _company: ICompany | null = await CompanyModel.findOne({
       username,
     }).populate("role");
 
-    if (!_company || _company?.role.name !== type)
-      return res.json({ ok: false });
+    if (!_company) return res.status(401).json({ ok: false });
 
     const matchPass = await comparePassword(_company.password, password);
 
-    if (!matchPass) return res.json({ ok: false });
+    if (!matchPass) return res.status(401).json({ ok: false });
+
+    const _drivers: IDriver[] = await DriverModel.find()
+      .populate("company")
+      .where({
+        company: _company,
+      });
 
     res.cookie("jid", createRefreshToken(_company), cookieConf);
 
-    return res.json({ data: _company, token: createAcessToken(_company) });
+    return res.json({
+      ok: true,
+      result: {
+        data: {
+          name: _company.name,
+          id: _company._id,
+          username: _company.username,
+          lat: _company.lat,
+          lng: _company.lng,
+          address: _company.address,
+          materials: _company.materials,
+          role: _company.role.name,
+          drivers: _drivers.map((d) => ({
+            name: d.name,
+            id: d._id,
+            plate: d.plate,
+          })),
+        },
+        token: createAcessToken(_company),
+      },
+    });
   }
 
-  return res.json({ ok: false });
+  return res.status(401).json({ ok: false });
 };
 
 export const signUp = async (
@@ -79,12 +125,12 @@ export const signUp = async (
     const _role2: IRole | null = await RoleModel.findOne({ name: type });
 
     if (_driver2 || !_company2 || !_role2) {
-      return res.json({ ok: false });
+      return res.status(401).json({ ok: false });
     }
 
     const _driver = new DriverModel({
-      lat,
-      lng,
+      lat: _company2.lat,
+      lng: _company2.lng,
       name,
       plate,
       material,
@@ -99,7 +145,28 @@ export const signUp = async (
 
     res.cookie("jid", createRefreshToken(driver_), cookieConf);
 
-    return res.json({ data: driver_ });
+    return res.json({
+      ok: true,
+      result: {
+        data: {
+          name: driver_.name,
+          id: driver_._id,
+          username: driver_.username,
+          plate: driver_.plate,
+          lat: driver_.lat,
+          lng: driver_.lng,
+          role: driver_.role.name,
+          company: {
+            name: driver_.company.name,
+            id: driver_.company._id,
+            address: driver_.company.address,
+            lat: driver_.company.lat,
+            lng: driver_.company.lng,
+          },
+        },
+        token: createAcessToken(driver_),
+      },
+    });
   }
 
   if (type === "company") {
@@ -109,7 +176,7 @@ export const signUp = async (
     const _role2: IRole | null = await RoleModel.findOne({ name: type });
 
     if (_company2 || !_role2) {
-      return res.json({ ok: false });
+      return res.status(401).json({ ok: false });
     }
 
     const _company = new CompanyModel({
@@ -128,8 +195,24 @@ export const signUp = async (
 
     res.cookie("jid", createRefreshToken(company_), cookieConf);
 
-    return res.json({ data: company_, token: createAcessToken(company_) });
+    return res.json({
+      ok: true,
+      result: {
+        data: {
+          name: company_.name,
+          id: company_._id,
+          username: company_.username,
+          lat: company_.lat,
+          lng: company_.lng,
+          address: company_.address,
+          materials: company_.materials,
+          role: company_.role.name,
+          drivers: [],
+        },
+        token: createAcessToken(company_),
+      },
+    });
   }
 
-  return res.json({ ok: false });
+  return res.status(401).json({ ok: false });
 };
