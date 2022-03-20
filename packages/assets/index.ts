c453ea09a12462a6ae8a31aed99c7797e55e1289
Italo Alfaro <bestor911@gmail.com>
2022-03-20 10:11:01 -0500

    feat: new image

--- packages/assets/index.ts
+++ packages/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as logo } from "./images/logo.png";
 export { default as avatar } from "./images/avatar.png";
 export { default as truck } from "./images/truck.png";
+export { default as truckDark } from "./images/truck-dark.png";
