5daea88f9309de1bb4dee34ea3b5b6b0be2296d6
Italo Alfaro <bestor911@gmail.com>
2022-03-19 20:31:34 -0500

    feat(web): register functionality

---
+++ apps/web/src/pages/Register/index.tsx
@@ -0,0 +1,185 @@
+import React, { useReducer } from "react";
+import { Btn, Container, TextInput, Txt } from "components/src/Elements";
+import { initialState, reducer } from "./helper";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { saveCompany } from "../../redux";
+import { FCompany } from "types";
+import { Navbar } from "components";
+import { Post } from "services";
+
+export const Register: React.FC = () => {
+  const navigation = useNavigate();
+  const [{ address, name, password, username }, dispatcher] = useReducer(
+    reducer,
+    initialState,
+  );
+  const dispatch = useDispatch();
+  return (
+    <Container
+      width="100%"
+      justify="center"
+      align="center"
+      heigh="100%"
+      bg="#0D1117"
+      direction="column"
+    >
+      <Navbar
+        logo={() => navigation("/")}
+        content={
+          <Container justify="flex-end" align="center" width="100%">
+            <Container
+              justify="space-between"
+              align="center"
+              width="fit-content"
+              margin="0px 5px"
+            >
+              <Txt fs="16px" color="#ffffff">
+                Already have an account?
+              </Txt>
+            </Container>
+            <Container
+              justify="space-between"
+              align="center"
+              width="fit-content"
+              margin="0px 5px"
+            >
+              <Txt
+                fs="16px"
+                color="#ff0000"
+                pointer
+                onClick={() => navigation("/login")}
+              >
+                Sign In
+              </Txt>
+            </Container>
+          </Container>
+        }
+      />
+      <Container
+        width="100%"
+        justify="center"
+        align="center"
+        heigh="100%"
+        bg="#0D1117"
+        direction="column"
+      >
+        <Txt fs="28px" bold color="#ffffff" margin="0px 0px 20px 0px">
+          Sign Up
+        </Txt>
+        <Container
+          bg="#C4C4C420"
+          justify="center"
+          heigh="350px"
+          width="350px"
+          align="center"
+          borderRadius="8px"
+          shadow
+          padding="10px"
+          direction="column"
+        >
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <TextInput
+              margin="15px 0px"
+              type="text"
+              color="#ffffff"
+              fs="16px"
+              width="100%"
+              borderBottom
+              borderBottomColor="red"
+              placerholderColor="#a3a3a3"
+              placeholder="Name"
+              value={name}
+              onChange={(e) =>
+                dispatcher({ type: "setName", payload: e.target.value })
+              }
+            />
+            <TextInput
+              margin="15px 0px"
+              type="text"
+              color="#ffffff"
+              fs="16px"
+              width="100%"
+              borderBottom
+              borderBottomColor="red"
+              placerholderColor="#a3a3a3"
+              placeholder="Address"
+              value={address}
+              onChange={(e) =>
+                dispatcher({ type: "setAddress", payload: e.target.value })
+              }
+            />
+            <TextInput
+              required
+              margin="15px 0px"
+              type="text"
+              color="#ffffff"
+              fs="16px"
+              width="100%"
+              borderBottom
+              borderBottomColor="red"
+              placerholderColor="#a3a3a3"
+              placeholder="Username"
+              value={username}
+              onChange={(e) =>
+                dispatcher({ type: "setUsername", payload: e.target.value })
+              }
+            />
+            <TextInput
+              required
+              margin="15px 0px"
+              type="password"
+              color="#ffffff"
+              fs="16px"
+              width="100%"
+              borderBottom
+              borderBottomColor="red"
+              placerholderColor="#a3a3a3"
+              placeholder="Password"
+              value={password}
+              onChange={(e) =>
+                dispatcher({ type: "setPassword", payload: e.target.value })
+              }
+            />
+            <Btn
+              type="submit"
+              bg="#f7160f"
+              width="100%"
+              height="30px"
+              borderRadius="4px"
+              margin="30px 0px 0px 0px"
+              onClick={async () => {
+                const res = await Post<{ ok: boolean; result: FCompany }>(
+                  "/auth/sign-up",
+                  {
+                    type: "company",
+                    name,
+                    password,
+                    username,
+                    address,
+                    lat: NaN,
+                    lng: NaN,
+                    materials: [],
+                  },
+                );
+                if (res.data.ok) {
+                  dispatch(saveCompany(res.data.result));
+                  dispatcher({ type: "clearAll" });
+                  navigation("/main");
+                }
+              }}
+            >
+              <Txt color="#ffffff" fs="16px" pointer>
+                Register
+              </Txt>
+            </Btn>
+          </form>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
