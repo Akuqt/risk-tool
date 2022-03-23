50b451f08723aa5eac756811a00ba1785454676e
Italo Alfaro <bestor911@gmail.com>
2022-03-22 21:56:32 -0500

    feat: static serve

---
+++ apps/web/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/*", function (_req, res) {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Frontend at: http://localhost:3000/");
+});
