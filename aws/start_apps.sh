15618abfae99e0cce2b0c38f1e573a610b7a615c
Italo Alfaro <bestor911@gmail.com>
2022-03-24 09:51:40 -0500

    fix: pm2 errors

--- aws/start_apps.sh
+++ aws/start_apps.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 cd /home/ubuntu/risk-tool
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 npm i -g pm2
+
+cmd="pm2 stop all"
+$cmd
+status=$?
+
+if [ $status -eq 0 ]; then
+  pm2 start all
+else
+  pm2 start release/web/index.js --name f
+  pm2 start release/api/index.js --name b
+fi
