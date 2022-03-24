1dca5af6616dac1f38891d3cd3877e03a773fbb4
Italo Alfaro <bestor911@gmail.com>
2022-03-23 21:53:47 -0500

    fix: node errors

--- aws/after_install.sh
+++ aws/after_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 cd /home/ubuntu/risk-tool
+
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
+npm i -g yarn
+yarn set version stable
 yarn install
 yarn build
