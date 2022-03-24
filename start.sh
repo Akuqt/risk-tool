1e5d16293858ca362bc55c969a7066f8c01a9d96
Italo Alfaro <bestor911@gmail.com>
2022-03-24 17:45:25 -0500

    chore: update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,6 @@
 
 # Use only in the EC2 instance
 
-cmd="pm2 stop all"
-$cmd
-status=$?
-
-if [ $status -eq 0 ]; then
-  pm2 start all
-  pm2 save --force
-else
-  pm2 start release/web/index.js --name frontend
-  pm2 start release/api/index.js --name backend
-  pm2 save --force
-fi
+cd ~/risk-tool/
+pm2 restart process.yml
+pm2 save --force
