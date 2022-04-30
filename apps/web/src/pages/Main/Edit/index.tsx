230a0f28fdac3b1bb3ba3b751a2de90dc917b4be
Italo Alfaro <bestor911@gmail.com>
2022-04-30 11:17:58 -0500

    fix: element's width

--- apps/web/src/pages/Main/Edit/index.tsx
+++ apps/web/src/pages/Main/Edit/index.tsx
@@ -3,9 +3,21 @@ import Select from "react-select";
 import { Container, Txt, TextInput, Btn } from "components/src/Elements";
 
 const options = [
-  { value: "carbondi", label: "Carbon disulfide" },
+  { value: "option1", label: "Option 1" },
   { value: "option2", label: "Option 2" },
   { value: "option3", label: "Option 3" },
+  { value: "option11", label: "Option 11" },
+  { value: "option21", label: "Option 21" },
+  { value: "option31", label: "Option 31" },
+  { value: "option12", label: "Option 12" },
+  { value: "option22", label: "Option 22" },
+  { value: "option32", label: "Option 32" },
+  { value: "option13", label: "Option 13" },
+  { value: "option23", label: "Option 23" },
+  { value: "option33", label: "Option 33" },
+  { value: "option14", label: "Option 14" },
+  { value: "option24", label: "Option 24" },
+  { value: "option34", label: "Option 34" },
 ];
 
 export const Edit: React.FC = () => {
@@ -30,13 +42,16 @@ export const Edit: React.FC = () => {
         direction="column"
       >
         <Txt fs="28px" bold color="#000000" margin="0px 0px 35px 0px">
-          Account Settings
+          Edit Information
         </Txt>
         <Container
           justify="center"
-          width="350px"
+          width="50%"
           align="center"
           direction="column"
+          style={{
+            maxWidth: 400,
+          }}
         >
           <Container
             width="100%"
@@ -71,7 +86,7 @@ export const Edit: React.FC = () => {
               borderRadius="4px"
             />
             <Txt fs="14px" bold color="#000000" margin="0px 0px 5px 0px">
-              Select the product to be transported
+              Materials
             </Txt>
             <div style={{ width: "99%", margin: "0px 0px 20px 0px" }}>
               <Select
@@ -87,7 +102,7 @@ export const Edit: React.FC = () => {
               width="99%"
               height="30px"
               borderRadius="4px"
-              margin="5px 0px 0px 0px"
+              margin="20px 0px 0px 0px"
             >
               <Txt color="#000000" fs="16px" pointer>
                 Save Changes
