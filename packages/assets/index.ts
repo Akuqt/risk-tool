1ddf512810a6a83bf8ff51ec058b8d0772bf4c6f
Italo Alfaro <bestor911@gmail.com>
2022-05-08 16:36:37 -0500

    feat: origin and destination icons

--- packages/assets/index.ts
+++ packages/assets/index.ts
@@ -2,3 +2,5 @@ export { default as logo } from "./images/logo.png";
 export { default as avatar } from "./images/avatar.png";
 export { default as truck } from "./images/truck.png";
 export { default as truckDark } from "./images/truck-dark.png";
+export { default as destinationIcon } from "./images/destination.svg";
+export { default as originIcon } from "./images/origin.svg";
