64e0a13ffd0dee84fa3cf4ed9047920d33cea4e0
Italo Alfaro <bestor911@gmail.com>
2022-05-14 17:57:16 -0500

    feat(api): best path controller

--- apps/api/src/controllers/path.controller.ts
+++ apps/api/src/controllers/path.controller.ts
@@ -1,9 +1,80 @@
+import { errors, getBestRoutePath } from "../lib";
 import { Response, Request } from "express";
 import { WazeAPI } from "waze-api";
-import { errors } from "../lib";
 import { Coord } from "types";
 
 const waze = new WazeAPI();
+
+type WazeAlt = Awaited<ReturnType<typeof waze.getPaths>>["alternatives"][0];
+
+const pathMap = (alt: WazeAlt) => ({
+  coords: alt.coords.map((coord) => ({
+    lat: coord.y,
+    lng: coord.x,
+  })) as Coord[],
+  time: alt.response.totalSeconds,
+  distance: alt.response.totalLength,
+});
+
+const fetchPath = async (from: Coord, to: Coord, n = 1) => {
+  const path = await waze.getPaths({
+    from: {
+      y: from.lat,
+      x: from.lng,
+    },
+    to: {
+      y: to.lat,
+      x: to.lng,
+    },
+    nPaths: n,
+    interval: 15,
+    arriveAt: true,
+    useCase: "LIVEMAP_PLANNING",
+  });
+
+  return path;
+};
+
+export const getBestPath = async (req: Request, res: Response) => {
+  const { origin, destination } = req.body as {
+    origin: Coord;
+    destination: Coord;
+  };
+
+  if (!origin || !destination) {
+    return res.status(400).json({ ok: false, error: errors.badRequest });
+  }
+
+  const { fixedPath, nextDestination, nextOrigin } = getBestRoutePath(
+    origin,
+    destination,
+  );
+
+  if (!fixedPath) {
+    return res.status(400).json({ ok: false, error: errors.generic });
+  }
+
+  const originPath = await fetchPath(origin, nextOrigin, 3);
+  const destinationPath = await fetchPath(destination, nextDestination, 3);
+
+  const result = {
+    fixedPath,
+    originPath: originPath.alternatives.map(pathMap),
+    destinationPath: destinationPath.alternatives.map(pathMap),
+  };
+
+  for (const a of result.originPath) {
+    console.log(a.distance);
+  }
+  for (const a of result.destinationPath) {
+    console.log(a.distance);
+  }
+
+  return res.status(200).json({
+    ok: true,
+    result,
+  });
+};
 
 export const getPath = async (
   req: Request<any, any, { points: { lat: number; lng: number }[] }>,
