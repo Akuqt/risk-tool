8257ee997fe208af7b5d8ca26b9254d6c7f13eee
Italo Alfaro <bestor911@gmail.com>
2022-05-14 22:45:18 -0500

    feat: slider

--- packages/components/src/Elements/index.tsx
+++ packages/components/src/Elements/index.tsx
@@ -222,3 +222,36 @@ export const Spinner = styled.div<{
     }
   }
 `;
+
+export const Slider = styled.input<{ color: string }>`
+  appearance: none;
+  width: 100%;
+  height: 10px;
+  background: #d3d3d3;
+  border-radius: 4px;
+  outline: none;
+  opacity: 0.7;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &::-webkit-slider-thumb {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: ${(p) => p.color};
+    cursor: pointer;
+  }
+
+  &::-moz-range-thumb {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: ${(p) => p.color};
+    cursor: pointer;
+  }
+`;
