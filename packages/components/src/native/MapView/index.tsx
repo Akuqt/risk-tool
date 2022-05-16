f34c1f25ca78f62f6ad138e77b4d84609b441224
Italo Alfaro <bestor911@gmail.com>
2022-05-15 19:47:41 -0500

    feat: poly render

--- packages/components/src/native/MapView/index.tsx
+++ packages/components/src/native/MapView/index.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
-import Map, { Marker } from "react-native-maps";
+import React from "react";
+import Map, { Marker, Polyline } from "react-native-maps";
 import mapStyle from "./style";
 import { View } from "react-native";
 import { Data } from "types";
 
 interface Props {
-  location: Data;
+  location?: Data;
+  polys?: { latitude: number; longitude: number }[][];
+  markers?: {
+    coords: { latitude: number; longitude: number };
+    info: {
+      address: string;
+      name: string;
+    };
+  }[];
 }
 
 export const MapView: React.FC<Props> = (props) => {
-  const [deltas, setDeltas] = useState({
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
-
   return (
     <View>
       <Map
@@ -21,28 +24,29 @@ export const MapView: React.FC<Props> = (props) => {
           width: "100%",
           height: "100%",
         }}
+        showsUserLocation
         customMapStyle={mapStyle}
-        region={{
-          latitude: props.location.lat,
-          longitude: props.location.lng,
-          latitudeDelta: deltas.latitudeDelta,
-          longitudeDelta: deltas.longitudeDelta,
-        }}
-        onRegionChangeComplete={(r) => {
-          setDeltas({
-            latitudeDelta: r.latitudeDelta,
-            longitudeDelta: r.longitudeDelta,
-          });
-        }}
       >
-        <Marker
-          coordinate={{
-            latitude: props.location.lat,
-            longitude: props.location.lng,
-          }}
-          title="Test"
-          description="Testing"
-        />
+        {props.markers &&
+          props.markers.length > 0 &&
+          props.markers.map((marker, i) => (
+            <Marker
+              key={i}
+              coordinate={marker.coords}
+              title={marker.info.name || ""}
+              description={marker.info.address || ""}
+            />
+          ))}
+        {props.polys &&
+          props.polys.length > 0 &&
+          props.polys.map((poly, i) => (
+            <Polyline
+              key={i}
+              coordinates={poly}
+              strokeColor="tomato"
+              strokeWidth={4}
+            />
+          ))}
       </Map>
     </View>
   );
