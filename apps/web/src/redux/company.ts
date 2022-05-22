3ec11cfed85c4922cbc3336240a1af63539d4197
Italo Alfaro <bestor911@gmail.com>
2022-05-21 22:21:37 -0500

    feat(web): new redux dispatchers

--- apps/web/src/redux/company.ts
+++ apps/web/src/redux/company.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { FCompany } from "types";
+import { FCompany, FDriver2, FLog2 } from "types";
 
 interface Init {
   company: FCompany;
@@ -32,9 +32,47 @@ export const companySlice = createSlice({
     clearCompany: (state) => {
       state.company = initialState.company;
     },
+    saveLogs: (state, action: PayloadAction<FLog2[]>) => {
+      state.company.logs = action.payload;
+    },
+    addLog: (state, action: PayloadAction<FLog2>) => {
+      if (state.company.logs.find((l) => l.id === action.payload.id)) return;
+      state.company.logs.unshift(action.payload);
+    },
+    filterLogs: (state, action: PayloadAction<string>) => {
+      state.company.logs = state.company.logs.filter(
+        (l) => l.id !== action.payload,
+      );
+    },
+    addDriver: (state, action: PayloadAction<FDriver2>) => {
+      state.company.drivers.push(action.payload);
+    },
+    updateDriverAndLogs: (
+      state,
+      action: PayloadAction<{ risk: number; id?: string }>,
+    ) => {
+      state.company.drivers = state.company.drivers.map((d) => {
+        if (d.id === action.payload.id) {
+          return { ...d, active: true };
+        }
+        return d;
+      });
+      state.company.lastRoutes.push({
+        risk: action.payload.risk,
+        date: new Date(),
+      });
+    },
   },
 });
 
-export const { clearCompany, saveCompany } = companySlice.actions;
+export const {
+  clearCompany,
+  saveCompany,
+  addLog,
+  saveLogs,
+  filterLogs,
+  addDriver,
+  updateDriverAndLogs,
+} = companySlice.actions;
 
 export default companySlice.reducer;
